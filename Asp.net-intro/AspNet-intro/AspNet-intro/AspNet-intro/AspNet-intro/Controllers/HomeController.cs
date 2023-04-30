02f311b33594a92af6dd737eafc37f20a49507c7
RustamovVugar <vugarar@code.edu.az>
2023-04-30 07:40:02 +0400

    added Asp.net folder

---
+++ Asp.net-intro/AspNet-intro/AspNet-intro/AspNet-intro/AspNet-intro/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            return View(nums);
+        }
+    }
+}
---
+++ Asp.net-intro/AspNet-intro/AspNet-intro/AspNet-intro/AspNet-intro/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
